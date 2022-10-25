2604671a635463b2720fa67cdfeba074e16aae22
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2022-10-25 12:16:35 +0200

    aws-adapter: Create a new logger for each instance instead of using complicated synchronized memoization

--- aws-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/FunctionInvoker.java
+++ aws-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/FunctionInvoker.java
@@ -3,7 +3,6 @@ package fr.axelop.agnosticserverlessfunctions;
 import java.util.logging.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
@@ -12,14 +11,12 @@ public class FunctionInvoker implements RequestHandler<APIGatewayV2HTTPEvent, AP
 
     private static final HttpRequestMapper REQUEST_MAPPER = new HttpRequestMapper();
     private static final HttpResponseMapper RESPONSE_MAPPER = new HttpResponseMapper();
-    private static final Logger LOGGER = Logger.getLogger(FunctionInvoker.class.getName());
-    private static final LoggerHandlerManager LOGGER_HANDLER_MANAGER = LoggerHandlerManager.getInstance(LOGGER);
 
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
-        final LambdaLogger lambdaLogger = context.getLogger();
-        LOGGER_HANDLER_MANAGER.addHandler(LambdaLoggerHandler.getInstance(lambdaLogger));
-        final Handler handler = HandlerLoader.getInstance(LOGGER).loadOrThrow();
-        final HttpResponse handlerResponse = handler.handle(REQUEST_MAPPER.map(request), LOGGER);
+        final Logger logger = Logger.getAnonymousLogger();
+        logger.addHandler(new LambdaLoggerHandler(context.getLogger()));
+        final Handler handler = HandlerLoader.getInstance(logger).loadOrThrow();
+        final HttpResponse handlerResponse = handler.handle(REQUEST_MAPPER.map(request), logger);
         return RESPONSE_MAPPER.map(handlerResponse);
     }
     
