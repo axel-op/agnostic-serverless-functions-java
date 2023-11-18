69f283a7a6ffb274fbba963e5d9592cf43851ce8
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2023-11-18 02:00:33 +0100

    Combine multi-values headers

--- aws-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/HttpResponseMapper.java
+++ aws-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/HttpResponseMapper.java
@@ -3,16 +3,26 @@ package fr.axelop.agnosticserverlessfunctions;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse.APIGatewayV2HTTPResponseBuilder;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 class HttpResponseMapper {
 
     public APIGatewayV2HTTPResponse map(HttpResponse handlerResponse) {
         final APIGatewayV2HTTPResponseBuilder builder = APIGatewayV2HTTPResponse.builder()
                 .withStatusCode(handlerResponse.getStatusCode())
-                .withMultiValueHeaders(handlerResponse.getHeaders());
+                .withMultiValueHeaders(handlerResponse.getHeaders())
+                .withHeaders(toCombinedValueHeaders(handlerResponse.getHeaders()));
         if (handlerResponse.getBody().isPresent()) {
             builder.withBody(handlerResponse.getBody().get());
-        }   
+        }
         return builder.build();
+    }
+
+    private static Map<String, String> toCombinedValueHeaders(Map<String, List<String>> multiValueHeaders) {
+        return multiValueHeaders.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> String.join(",", e.getValue())));
     }
     
 }
--- azure-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/HttpResponseMapper.java
+++ azure-adapter/src/main/java/fr/axelop/agnosticserverlessfunctions/HttpResponseMapper.java
@@ -2,6 +2,7 @@ package fr.axelop.agnosticserverlessfunctions;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
@@ -12,15 +13,18 @@ class HttpResponseMapper {
     HttpResponseMessage map(HttpResponse handlerResponse, HttpRequestMessage<?> azureRequest) {
         final HttpStatus httpStatus = HttpStatus.valueOf(handlerResponse.getStatusCode());
         final HttpResponseMessage.Builder builder = azureRequest.createResponseBuilder(httpStatus);
-        for (Map.Entry<String, List<String>> headerEntry : handlerResponse.getHeaders().entrySet()) {
-            for (String value : headerEntry.getValue()) {
-                builder.header(headerEntry.getKey(), value);
-            }
+        for (Map.Entry<String, String> headerEntry : toCombinedValueHeaders(handlerResponse.getHeaders()).entrySet()) {
+            builder.header(headerEntry.getKey(), headerEntry.getValue());
         }
         if (handlerResponse.getBody().isPresent()) {
             builder.body(handlerResponse.getBody().get());
         }   
         return builder.build();
+    }
+
+    private static Map<String, String> toCombinedValueHeaders(Map<String, List<String>> multiValueHeaders) {
+        return multiValueHeaders.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> String.join(",", e.getValue())));
     }
     
 }
